33afb0953c3cfbacee43309ea22a5c565cffbdf2
Cristina Salvo <csalvo783@gmail.com>
2017-12-31 01:11:10 -0500

    adding styles

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ function initClient() {
 function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
         authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
+        signoutButton.style.display = 'none';
         listFiles();
     } else {
         authorizeButton.style.display = 'block';
