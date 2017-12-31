aa1a020256ee7d624ddcfade94ceec0a67273077
Cristina Salvo <csalvo783@gmail.com>
2017-12-31 13:52:46 -0500

    remove href

--- app.js
+++ app.js
@@ -75,9 +75,8 @@ function handleSignoutClick(event) {
 function appendImg(pic) {
     pre = document.getElementById('content');
     image = "<img class='pic' src='"
-    imageLink = "' target='_blank' href='"
     imageclose = "'>"
-    pre.innerHTML += image + pic + imageLink + pic + imageclose;
+    pre.innerHTML += image + pic + imageclose;
 }
 
 /**
